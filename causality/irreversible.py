b0ab72fcf01a3b605cb92ac3c2d51f11e125c8c7
AttraQter-Labs <nic_hensley@proton.me>
2026-01-17 16:14:11 -0800

    Create find_irreversible function stub
    Added a stub function for irreversibility detection.

---
+++ causality/irreversible.py
@@ -0,0 +1,7 @@
+def find_irreversible(*args, **kwargs):
+    """Irreversibility detector (MVP stub).
+
+    Accepts arbitrary arguments and returns an empty set.
+    Provided to maintain deterministic test imports.
+    """
+    return set()
